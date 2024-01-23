3157da65fe3f6ee69ab4b26a27afb79caaa0438a
sasaplus1 <sasaplus1@gmail.com>
2024-01-24 00:12:24 +0900

    add chapter-02

---
+++ chapter-02/echor/tests/cli.rs
@@ -0,0 +1,18 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+
+    cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn dies_no_args() {
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+}
