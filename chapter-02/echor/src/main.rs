117d9d3b698180b701f1f8fbecbbdba5d69eaeb0
sasaplus1 <sasaplus1@gmail.com>
2024-01-26 09:42:49 +0900

    fix

--- chapter-02/echor/src/main.rs
+++ chapter-02/echor/src/main.rs
@@ -27,7 +27,7 @@ fn main() {
     let text = matches.values_of_lossy("text").unwrap();
     let omit_newline = matches.is_present("omit_newline");
 
-    let ending = if omit_newline { "\n" } else { "" };
+    let ending = if omit_newline { "" } else { "\n" };
 
-    println!("{}{}", text.join(" "), ending);
+    print!("{}{}", text.join(" "), ending);
 }
